4d20a6a790657171259d3607b380eb5a848da8ef
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-08-10 16:11:30 -0400

    ajustes

--- resources/views/pages/report/index.blade.php
+++ resources/views/pages/report/index.blade.php
@@ -149,6 +149,15 @@ function details(image, latitude, longitude){
         //createMap2(myLatLng);
         console.log(longitude+"-"+latitude)
         var mymap = L.map('mapid').setView([latitude, longitude], 13);
+
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2Fib2xlYWwxMjMiLCJhIjoiY2tqYWh1bmYxMW9zOTJ6bnllZDJ5cHU1ZiJ9.dWucc-gHwan1ANwOgt2fFQ', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiZ2Fib2xlYWwxMjMiLCJhIjoiY2tqYWh1bmYxMW9zOTJ6bnllZDJ5cHU1ZiJ9.dWucc-gHwan1ANwOgt2fFQ'
+        }).addTo(mymap);
     }
     
     if(image != ""){
